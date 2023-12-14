d3b756bc8f9b234e429ce7ecb6afe091a5c964ad
eren595 <eren595@BPC12>
2023-12-14 14:52:09 +0300

    Proje dosyası ekle.

---
+++ OkulPersoneli/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OkulPersoneli
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ogretmen ogr = new Ogretmen
+            {
+                Ad = "Ahmet",
+                Soyad = "Öz",
+                Brans = "Matematik"
+            };
+            OkulPersoneli per = ogr;
+            Console.WriteLine(per.Ad);
+        }
+    }
+    public class OkulPersoneli
+    {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+    }
+    public class Ogretmen : OkulPersoneli
+    {
+        public string Brans { get; set; }
+    }
+}
